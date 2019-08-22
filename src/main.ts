7a84082454cd7c7bd3322d7720be106e63a0494a
Kevin Peters <kwpeters@gmail.com>
2019-08-22 08:28:02 -0400

    Created project.

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import * as path from "path";
+import * as fs from "fs";
+import * as pegjs from "pegjs";
+
+const grammarFile = path.join(__dirname, "pegGrammar.peg");
+const pegGrammar = fs.readFileSync(grammarFile, "utf8");
+
+const parser = pegjs.generate(pegGrammar, {output: "parser"});
+const input = "b.d.e";
+try {
+    const result = parser.parse(input);
+    console.log("Parse succeeded.");
+    console.log(result);
+}
+catch (err) {
+    console.error("Parse failed.");
+    console.error(err);
+}
