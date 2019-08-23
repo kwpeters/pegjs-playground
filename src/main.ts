b45d42e0e5e24eba6c87632f81aa1d86867ace67
Kevin Peters <kwpeters@gmail.com>
2019-08-23 06:17:01 -0400

    Renamed file.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as path from "path";
 import * as fs from "fs";
 import * as pegjs from "pegjs";
 
-const grammarFile = path.join(__dirname, "pegGrammar.peg");
+const grammarFile = path.join(__dirname, "predicateGrammar.peg");
 const pegGrammar = fs.readFileSync(grammarFile, "utf8");
 
 const parser = pegjs.generate(pegGrammar, {output: "parser"});
